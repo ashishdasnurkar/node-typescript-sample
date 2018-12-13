c8fe3106a252fa47369c63d7d460271f0b4b37ae
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2018-12-13 15:22:07 +0800

    Add a Thing class and demo class

---
+++ src/thing.ts
@@ -0,0 +1,35 @@
+import { INamed } from "./common";
+import { Animal } from "./animal";
+import { Color } from "./color";
+
+export class Thing implements INamed {
+  
+  private _animal: Animal;
+  private _color: Color;
+
+  public get animal(): Animal {
+    return this._animal;
+  }
+
+  public get color(): Color {
+    return this._color;
+  }
+
+  constructor(animal: Animal, color: Color) {
+    this._animal = animal;
+    this._color = color;
+  }
+
+  public get name(): string {
+    return `${this._color.name} ${this._animal.name}`;
+  }
+}
+  function pickOne<T>(list : Array<T>):T {
+    var idx = Math.floor(Math.random() * list.length);
+    return list[idx];
+  }
+
+  export var makeThing = (colorList: Array<Color>, animalList: Array<Animal>) => {
+    return new Thing(pickOne(colorList), pickOne(animalList));
+  };
+
